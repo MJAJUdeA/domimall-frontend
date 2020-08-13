56c098e08983587da8b0c179500cc7f80e9f7caa
manaranjoc <angel.naranjo@udea.edu.co>
2020-08-13 01:26:47 -0500

    Add cucumber style, better assertions and example cucumber

---
+++ features/example.feature
@@ -0,0 +1,5 @@
+Feature: Example Feature
+    Scenario: Example of range
+        Given An initial Hello
+        When print
+        Then print Hello World
---
+++ features/step_definitions/steps.js
@@ -0,0 +1,16 @@
+const { Given, When, Then } = require('cucumber')
+
+Given('An initial Hello', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('print', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('print Hello World', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
