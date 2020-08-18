d18dcc7ee82e3e932071c2f2dd2f43c062a97d1c
Andres Grisales <andres.grisalesgg@gmail.com>
2020-08-17 20:45:35 -0500

    Add whole steps for creating commerce

---
+++ features/step_definitions/step_create_commerce.js
@@ -0,0 +1,74 @@
+const { Given, When, Then } = require('cucumber')
+
+Given('Commerce {string} is already created', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('{string} isn\'t an Admin', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('a Commerce called {string}', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('{string} isn\'t on SIC', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('ensure {string} doesn\'t exist on SIC', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('the notification should be {string}', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('{string} is on SIC', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+
+When('check {string} on SIC', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('ensure {string} exists on SIC', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+
+Given('the Commerce doesn\'t have a {string}', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+
+Given('{string} is an Admin', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('the Commerce have all data', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('create a Commerce', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('ensure Commerce is created', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
